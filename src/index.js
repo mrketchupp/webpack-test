3620420e5ae50a81b5dc867acb6e18f0dc884745
Kevin Santiago <sonicthe1111@gmail.com>
2023-03-10 02:08:16 -0600

    Babel, Javascript, HTML y produccion de webpack configurado

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log('Beidou ❤️‍🔥');
+
+(async function App() {
+    const main = document.getElementById('main');
+    const title = document.createElement('h1');
+    title.textContent = 'Beidou ❤️‍🔥';
+    main.appendChild(title);
+})();
